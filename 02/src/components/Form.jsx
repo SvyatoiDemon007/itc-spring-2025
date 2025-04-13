aee7b1520b641491ca2deb908afe3cd7cfba54ab
SvyatoiDemon007 <147990436+SvyatoiDemon007@users.noreply.github.com>
2025-04-13 18:05:41 +0500

    Update Form.jsx

--- 02/src/components/Form.jsx
+++ 02/src/components/Form.jsx
@@ -7,26 +7,45 @@ export const Form = ({ row, setRows, setCurrentRow }) => {
   const cityRef = useRef();
 
   useEffect(() => {
-  }, []);
+    if (row) {
+      nameRef.current.value = name;
+      cityRef.current.value = city;
+    }
+  }, [row]);
 
   const handleSave = () => {
+    if (id !== undefined) {
+      setRows((prevRows) => 
+        prevRows.map((r) => 
+          r.id === id ? { ...r, name: nameRef.current.value, city: cityRef.current.value } : r
+        )
+      );
+      setCurrentRow(-1);
+    }
   };
 
   return (
     <div className={classes.form}>
-      <div className={classes.row}>
-        <label className={classes.label}>Имя</label>
-        <input className={classes.input} ref={nameRef} defaultValue={name} />
-      </div>
-      <div className={classes.row}>
-        <label className={classes.label}>Город</label>
-        <input className={classes.input} ref={cityRef} defaultValue={city} />
-      </div>
-      <div className={classes.row}>
-        <button className={classes.button} onClick={handleSave}>
-          Сохранить
-        </button>
-      </div>
+      {id !== undefined ? (
+        <>
+          <div className={classes.row}>
+            <label className={classes.label}>Имя</label>
+            <input className={classes.input} ref={nameRef} />
+          </div>
+          <div className={classes.row}>
+            <label className={classes.label}>Город</label>
+            <input className={classes.input} ref={cityRef} />
+          </div>
+          <div className={classes.row}>
+            <button className={classes.button} onClick={handleSave}>
+              Сохранить
+            </button>
+          </div>
+        </>
+      ) : (
+        <p>Выберите строку для редактирования</p>
+      )}
     </div>
   );
 };
+
