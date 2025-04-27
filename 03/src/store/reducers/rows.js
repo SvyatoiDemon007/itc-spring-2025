87181a96170f271f462d64afcfb2d37bc3628e17
SvyatoiDemon007 <147990436+SvyatoiDemon007@users.noreply.github.com>
2025-04-27 15:01:44 +0500

    Update rows.js

--- 03/src/store/reducers/rows.js
+++ 03/src/store/reducers/rows.js
@@ -2,22 +2,27 @@ import { createSlice, createSelector } from "@reduxjs/toolkit";
 
 const initialState = {
   rows: [
-    { id: 1, name: "Петр Сидоров", city: "Екатеринбург" },
-    { id: 2, name: "Иван Сидоров", city: "Екатеринбург" },
-    { id: 3, name: "Сидор Сидоров", city: "Екатеринбург" },
-    { id: 4, name: "Иннокентий Сидоров", city: "Екатеринбург" },
+    { id: '1', name: "Петр Сидоров", city: "Екатеринбург" },
+    { id: '2', name: "Иван Сидоров", city: "Екатеринбург" },
+    { id: '3', name: "Сидор Сидоров", city: "Екатеринбург" },
+    { id: '4', name: "Иннокентий Сидоров", city: "Екатеринбург" },
   ],
-}
-
-const getRows = (state) => state.rows;
+};
 
 export const rowsSlice = createSlice({
   name: "rows",
   initialState,
   reducers: {
     setRow: (state, action) => {
+      const { id, name, city } = action.payload;
+      const index = state.rows.findIndex(row => row.id === id);
+      if (index !== -1) {
+        state.rows[index] = { id, name, city };
+      }
     }
-  },
-  selectors: {
-  },
+  }
 });
+
+const getRows = (state) => state.rows.rows;
+
+rowsSlice.selectors = { getRows };
