690e58435f0c529406dd759f36178749142fca87
SvyatoiDemon007 <147990436+SvyatoiDemon007@users.noreply.github.com>
2025-05-03 16:41:19 +0500

    Update Cell.jsx

--- 04/src/components/Cell.jsx
+++ 04/src/components/Cell.jsx
@@ -1,16 +1,23 @@
 import classes from './Cell.module.css';
 import clsx from 'clsx';
+import { useDispatch, useSelector } from 'react-redux';
+import { setPosition } from '../store/reducers/KnightSlice';
+import { Knight } from './Knight';
 
 export const Cell = ({ column, row }) => {
+  const dispatch = useDispatch();
+  const knightRow = useSelector((state) => state.knight.row);
+  const knightColumn = useSelector((state) => state.knight.column);
+  
   const color = (column % 2 === row % 2) ? 'white' : 'black';
 
   const handleClick = () => {
+    dispatch(setPosition({ row, column }));
   }
 
   return (
-    <div className={clsx(classes.cell, classes[color])}
-      onClick={handleClick}
-    >
+    <div className={clsx(classes.cell, classes[color])} onClick={handleClick}>
+      {knightRow === row && knightColumn === column && <Knight />}
     </div>
-  )
+  );
 }
