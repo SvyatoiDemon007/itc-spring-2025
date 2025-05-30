c6361e97756c36660ceb455a3f2a1ec7fa7c67e5
SvyatoiDemon007 <147990436+SvyatoiDemon007@users.noreply.github.com>
2025-05-30 15:55:26 +0500

    Update items.js

--- 14/src/store/slices/items.js
+++ 14/src/store/slices/items.js
@@ -10,6 +10,18 @@ export const itemsSlice = createSlice({
   name: 'items',
   initialState,
   reducers: {
+    moveDisk: (state, action) => {
+      const { from, to } = action.payload;
+      const fromPole = state[from];
+      const toPole = state[to];
+      if (fromPole.length === 0) return;
+      const disk = fromPole[fromPole.length - 1];
+      const topToDisk = toPole[toPole.length - 1];
+      if (!topToDisk || topToDisk > disk) {
+        fromPole.pop();
+        toPole.push(disk);
+      }
+    }
   },
   selectors: {
     getItems: (state) => state,
