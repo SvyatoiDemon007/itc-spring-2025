7b634644f16b437b6f772ea59295eaf2e83f2d10
SvyatoiDemon007 <147990436+SvyatoiDemon007@users.noreply.github.com>
2025-05-30 15:55:56 +0500

    Update Pole.jsx

--- 14/src/components/Pole.jsx
+++ 14/src/components/Pole.jsx
@@ -1,26 +1,40 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { moveSlice } from '../store/slices/move'
-import classes from './Pole.module.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { moveSlice } from '../store/slices/move';
 import { itemsSlice } from '../store/slices/items';
 import { Item } from './Item';
+import classes from './Pole.module.css';
 
 export const Pole = ({ id }) => {
   const { setSource, dropSource } = moveSlice.actions;
+  const { moveDisk } = itemsSlice.actions;
+  const { getSource } = moveSlice.selectors;
   const { getItems } = itemsSlice.selectors;
-  const dispatch = useDispatch();
 
+  const dispatch = useDispatch();
+  const source = useSelector(getSource);
   const items = useSelector(getItems)[id] || [];
 
   const handleClick = () => {
-  }
+    if (source === null) {
+      dispatch(setSource(id));
+    } else {
+      if (source !== id) {
+        dispatch(moveDisk({ from: source, to: id }));
+      }
+      dispatch(dropSource());
+    }
+  };
 
   const renderItems = () => {
-    return items.map((itemId) => <Item number={itemId} />);
-  }
+    return items.map((itemId, index) => (
+      <Item key={index} number={itemId} />
+    ));
+  };
 
   return (
-    <div className={ classes.pole } onClick={handleClick}>
-      { renderItems() }
+    <div className={classes.pole} onClick={handleClick}>
+      {renderItems()}
     </div>
-  )
-}
+  );
+};
