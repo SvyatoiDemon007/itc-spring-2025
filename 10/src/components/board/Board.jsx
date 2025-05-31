e70579058e25251151ae1e4f8e9a54b1e49dc091
SvyatoiDemon007 <147990436+SvyatoiDemon007@users.noreply.github.com>
2025-06-01 02:35:20 +0500

    Update Board.jsx

--- 10/src/components/board/Board.jsx
+++ 10/src/components/board/Board.jsx
@@ -1,33 +1,40 @@
-import classes from './Board.module.css'
-import { Row } from './Row';
-import { Cell } from './Cell';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { positionSlice } from '../../store/slices/position';
 import { bindSlice } from '../../store/slices/bind';
+import { positionSlice } from '../../store/slices/position';
+import { Row } from './Row';
+import { Cell } from './Cell';
+import classes from './Board.module.css';
 
-const range = () => [ 0, 1, 2 ];
+const range = () => [0, 1, 2];
 
 export const Board = () => {
   const dispatch = useDispatch();
   const { moveDir } = positionSlice.actions;
-  const { getBinds } = bindSlice.selectors;
-  const binds = useSelector(getBinds);
-  const { up, down, left, right } = binds;
+  const binds = useSelector(bindSlice.selectors.getBinds);
 
-  // TODO: useEffect
+  useEffect(() => {
+    const handler = (e) => {
+      const directionEntry = Object.entries(binds).find(([dir, key]) => key === e.code);
+      if (directionEntry) {
+        const [dir] = directionEntry;
+        dispatch(moveDir({ direction: dir.toUpperCase() }));
+      }
+    };
+
+    document.addEventListener('keydown', handler);
+    return () => document.removeEventListener('keydown', handler);
+  }, [binds, dispatch]);
 
   return (
     <div className={classes.board}>
-      {
-        range().map((row) => {
-          return (
-            <Row>
-              { range().map((column) => <Cell row={row} column={column}/>) }
-            </Row>
-          )
-        })
-      }
+      {range().map((row) => (
+        <Row key={row}>
+          {range().map((column) => (
+            <Cell key={`${row}-${column}`} row={row} column={column} />
+          ))}
+        </Row>
+      ))}
     </div>
   );
-}
+};
