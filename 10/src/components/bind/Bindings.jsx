b9803dd2c13f65c809fbfb043cec081f9f8371a5
SvyatoiDemon007 <147990436+SvyatoiDemon007@users.noreply.github.com>
2025-06-01 02:34:35 +0500

    Update Bindings.jsx

--- 10/src/components/bind/Bindings.jsx
+++ 10/src/components/bind/Bindings.jsx
@@ -1,23 +1,26 @@
-import classes from './Bindings.module.css'
-import { Bind } from './Bind'
-import { useEffect } from 'react'
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { bindSlice } from '../../store/slices/bind';
-import { useDispatch } from 'react-redux';
 import { positionSlice } from '../../store/slices/position';
+import { Bind } from './Bind';
+import classes from './Bindings.module.css';
 
 export const Bindings = () => {
-  const { setBind } = bindSlice.actions;
-  const { setPause } = positionSlice.actions;
   const dispatch = useDispatch();
+  const { getDirection } = bindSlice.selectors;
+  const { setBind } = bindSlice.actions;
+  const currentDirection = useSelector(getDirection);
 
   useEffect(() => {
     const bindHandler = (e) => {
-    }
+      if (currentDirection) {
+        dispatch(setBind({ direction: currentDirection, key: e.code }));
+      }
+    };
 
     document.addEventListener('keydown', bindHandler);
-
     return () => document.removeEventListener('keydown', bindHandler);
-  }, []);
+  }, [currentDirection, dispatch]);
 
   return (
     <div className={classes.bindings}>
@@ -26,5 +29,5 @@ export const Bindings = () => {
       <Bind direction="left" />
       <Bind direction="right" />
     </div>
-  )
-}
+  );
+};
