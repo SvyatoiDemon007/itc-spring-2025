cdb93e09d6229325091f5c970643312fe04aa17f
SvyatoiDemon007 <147990436+SvyatoiDemon007@users.noreply.github.com>
2025-06-01 00:39:29 +0500

    Update Check.jsx

--- 08/src/components/figures/Check.jsx
+++ 08/src/components/figures/Check.jsx
@@ -1,21 +1,26 @@
-import classes from './Check.module.css'
-import clsx from 'clsx'
+import classes from './Check.module.css';
+import clsx from 'clsx';
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { selectedSlice } from "../../store/reducers/selected";
 import { Figure } from './Figure';
 
 export const Check = ({ slug }) => {
   const { getSelected } = selectedSlice.selectors;
+  const { setSelected } = selectedSlice.actions;
 
-  const isSelected = slug === useSelector(getSelected);
+  const selected = useSelector(getSelected);
+  const dispatch = useDispatch();
+
+  const isSelected = slug === selected;
 
   const handleClick = () => {
-  }
+    dispatch(setSelected(slug));
+  };
 
   return (
-    <div className={clsx({[classes.selected]: isSelected})} onClick={handleClick}>
+    <div className={clsx({ [classes.selected]: isSelected })} onClick={handleClick}>
       <Figure slug={slug} />
     </div>
   );
-}
+};
