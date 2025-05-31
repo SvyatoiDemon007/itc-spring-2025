7d3c77749380f0669a36a47e5423dc846305b63f
SvyatoiDemon007 <147990436+SvyatoiDemon007@users.noreply.github.com>
2025-06-01 00:37:46 +0500

    Update Figure.jsx

--- 08/src/components/figures/Figure.jsx
+++ 08/src/components/figures/Figure.jsx
@@ -1,6 +1,8 @@
-export const Figure = () => {
-  return (
-    <>
-    </>
-  )
-}
+import { allFigures } from './registry';
+
+export const Figure = ({ slug }) => {
+  if (!slug || !allFigures[slug]) return null;
+
+  const Component = allFigures[slug];
+  return <Component />;
+};
