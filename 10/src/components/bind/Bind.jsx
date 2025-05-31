62340d7d7254a01d22f12537e1463a01e6792d7e
SvyatoiDemon007 <147990436+SvyatoiDemon007@users.noreply.github.com>
2025-06-01 02:34:04 +0500

    Update Bind.jsx

--- 10/src/components/bind/Bind.jsx
+++ 10/src/components/bind/Bind.jsx
@@ -1,28 +1,24 @@
-import { useEffect, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { bindSlice } from '../../store/slices/bind'
-import classes from './Bind.module.css'
-import { positionSlice } from '../../store/slices/position';
+import { bindSlice } from '../../store/slices/bind';
+import classes from './Bind.module.css';
 
 export const Bind = ({ direction }) => {
   const dispatch = useDispatch();
   const { getBinds, getDirection } = bindSlice.selectors;
-  const { setBind, selectDirection } = bindSlice.actions;
-  const { setPause } = positionSlice.actions;
-  const cdirection = useSelector(getDirection);
-  const binds = useSelector(getBinds);
-  const bind = binds[direction];
-  const waiting = direction === cdirection;
+  const { selectDirection } = bindSlice.actions;
+
+  const currentDirection = useSelector(getDirection);
+  const bindings = useSelector(getBinds);
+  const waiting = direction === currentDirection;
 
   const handleClick = () => {
-  }
+    dispatch(selectDirection(direction));
+  };
 
   return (
     <div className={classes.bind} onClick={handleClick}>
-      { direction.toLocaleUpperCase() }:&nbsp;
-      { bind }
-
-      { waiting ? <span className={classes.waiting}>waiting</span> : null }
+      {direction.toUpperCase()}: {bindings[direction]}
+      {waiting && <span className={classes.waiting}>waiting</span>}
     </div>
-  )
-}
+  );
+};
