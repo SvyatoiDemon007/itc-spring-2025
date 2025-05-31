0d21d2344f01e4319363621eb00639449e8c8b40
SvyatoiDemon007 <147990436+SvyatoiDemon007@users.noreply.github.com>
2025-05-31 17:22:12 +0500

    Update Cell.jsx

--- 04/src/components/Cell.jsx
+++ 04/src/components/Cell.jsx
@@ -8,28 +8,27 @@ export const Cell = ({ column, row }) => {
   const dispatch = useDispatch();
   const knightRow = useSelector((state) => state.knight.row);
   const knightColumn = useSelector((state) => state.knight.column);
-  
+
   const color = (column % 2 === row % 2) ? 'white' : 'black';
 
+  const isValidMove = (newRow, newCol) => {
+    const rowDiff = Math.abs(newRow - knightRow);
+    const colDiff = Math.abs(newCol - knightColumn);
+    return (rowDiff === 2 && colDiff === 1) || (rowDiff === 1 && colDiff === 2);
+  };
+
   const handleClick = () => {
-    dispatch(setPosition({ row, column }));
-  }
+    if (isValidMove(row, column)) {
+      dispatch(setPosition({ row, column }));
+    }
+  };
 
   return (
-    <div className={clsx(classes.cell, classes[color])} onClick={handleClick}>
+    <div
+      className={clsx(classes.cell, classes[color])}
+      onClick={handleClick}
+    >
       {knightRow === row && knightColumn === column && <Knight />}
     </div>
   );
-  const isValidMove = (newRow, newCol) => {
-  const rowDiff = Math.abs(newRow - knightRow);
-  const colDiff = Math.abs(newCol - knightColumn);
-  return (rowDiff === 2 && colDiff === 1) || (rowDiff === 1 && colDiff === 2);
-}
-
-const handleClick = () => {
-  if (isValidMove(row, column)) {
-    dispatch(setPosition({ row, column }));
-  }
-}
-
-}
+};
