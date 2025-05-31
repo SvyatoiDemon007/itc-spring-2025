f67fe1414f85325c81f2313664b202b1a1b945ef
SvyatoiDemon007 <147990436+SvyatoiDemon007@users.noreply.github.com>
2025-06-01 02:19:57 +0500

    Update Graph.jsx

--- 09/src/components/Graph.jsx
+++ 09/src/components/Graph.jsx
@@ -5,12 +5,14 @@ export const Graph = ({ population }) => {
     return null;
   }
 
+  const years = population.map((p) => p.year);
+  const values = population.map((p) => Number(p.value));
+
   return (
     <BarChart
-
-
-
+      xAxis={[{ scaleType: 'band', data: years }]}
+      series={[{ data: values, label: 'Население' }]}
       height={600}
     />
-  )
-}
+  );
+};
