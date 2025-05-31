4d139ecfd448ad9ff69cdb5d9005c466e2182bc5
SvyatoiDemon007 <147990436+SvyatoiDemon007@users.noreply.github.com>
2025-06-01 00:44:13 +0500

    Create registry.js

---
+++ 08/src/components/figures/registry.js
@@ -0,0 +1,30 @@
+import { BlackKing } from './BlackKing';
+import { BlackQueen } from './BlackQueen';
+import { BlackRook } from './BlackRook';
+import { BlackBishop } from './BlackBishop';
+import { BlackKnight } from './BlackKnight';
+import { BlackPawn } from './BlackPawn';
+
+import { WhiteKing } from './WhiteKing';
+import { WhiteQueen } from './WhiteQueen';
+import { WhiteRook } from './WhiteRook';
+import { WhiteBishop } from './WhiteBishop';
+import { WhiteKnight } from './WhiteKnight';
+import { WhitePawn } from './WhitePawn';
+
+
+export const allFigures = {
+  'black-king': BlackKing,
+  'black-queen': BlackQueen,
+  'black-rook': BlackRook,
+  'black-bishop': BlackBishop,
+  'black-knight': BlackKnight,
+  'black-pawn': BlackPawn,
+
+  'white-king': WhiteKing,
+  'white-queen': WhiteQueen,
+  'white-rook': WhiteRook,
+  'white-bishop': WhiteBishop,
+  'white-knight': WhiteKnight,
+  'white-pawn': WhitePawn,
+};
